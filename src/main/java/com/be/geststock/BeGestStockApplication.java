4470517b770f7d81a7e1b22f12c590075df23d89
Stanlay HOUNNOUVI <stanlayhounnouvi@gmail.com>
2023-04-15 09:20:10 +0100

    Be_GestStock First commit

---
+++ src/main/java/com/be/geststock/BeGestStockApplication.java
@@ -0,0 +1,13 @@
+package com.be.geststock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGestStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGestStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/geststock/BeGestStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.geststock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGestStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
